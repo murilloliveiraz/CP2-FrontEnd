6d577621d30c22ea6bd78b7f3124f45209414d8b
DaviGarcia00 <drgarcia035@gmail.com>
2024-04-30 12:45:20 -0300

    login html criado com js

--- script.js
+++ script.js
@@ -126,3 +126,18 @@ function calcTotalPrice(){
         total.innerHTML = `R$ ${(input * 108).toFixed(2)}`
     }
 }
+
+
+function logar(){
+    
+    var login = document.getElementById("login").value;
+    var senha = document.getElementById("senha").value;
+
+        if(login == '1234' && senha == '1234'){
+            alert('Login efetuado com sucesso!');
+            location.href = "index.html";
+        }else{
+            alert('Login ou senha inválidos!');
+        }
+
+}
